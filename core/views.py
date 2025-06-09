c609b56e8a105ebae05a870d55d0b72d36a5efeb
Ivan D.S. <ivandiazsanchez1996@gmail.com>
2025-06-09 22:00:22 +0200

    Fixed path error

--- core/views.py
+++ core/views.py
@@ -11,3 +11,14 @@ def index(response):
         return render(response,"index.html")
     except TemplateDoesNotExist:
         return HttpResponse("Backend OK. Posa en marxa el frontend seguint el README.")
+    
+def protected_serve(request, path, document_root=None, show_indexes=False):
+    full_path = os.path.join(document_root, path)
+
+    # If the path is a directory, deny access
+    if os.path.isdir(full_path):
+        raise PermissionDenied()
+        #return HttpResponseForbidden("Accés denegat.")
+
+    # Otherwise serve the file normally
+    return serve(request, path, document_root=document_root, show_indexes=show_indexes)
--- gamevault/urls.py
+++ gamevault/urls.py
@@ -18,7 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from core import views
-from ninja import NinjaAPI
 from django.conf import settings
 from django.conf.urls.static import static
 
